5ce04b3d0dc0b86ecb348a4b45a4d0e4fb8f50be
starfruit0000 <45466997+starfruit0000@users.noreply.github.com>
2021-05-11 14:24:31 +0900

    배열로 5*5줄 쓰기(주석 추가).cpp
    주석 추가했음
    앞으로는 이렇게 만들자

--- 배열로 5*5줄 쓰기.cpp
+++ 배열로 5*5줄 쓰기.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 int main()
 {
-	int nNumber[24];
-	int nInsideNumber = 0;
-	int nLineNumber = 0;
-	int nInsideNumPlus = 0;
+	int nNumber[24];//24개의 nNumber 변수들
+	int nInsideNumber = 0;//[] 안에 들어가는 숫자
+	int nLineNumber = 0;//현재 줄에 몇 개의 글자가 들어갔는가?
+	int nInsideNumPlus = 0;//nLineNumber + 1
 
 
 	nInsideNumber = 0;
@@ -19,20 +19,20 @@ int main()
 	nNumber[nInsideNumber] = nInsideNumPlus;
 	while (nInsideNumber < 25)
 	{
-		if (nLineNumber < 6)
+		if (nLineNumber < 6)//만약 현재 줄에 있는 글자 수가 6보다 작다면
 		{
-			cout << nInsideNumPlus;
-			cout << "\t";
-			nLineNumber++;
+			cout << nInsideNumPlus;//nLineNumber + 1 출력
+			cout << "\t";//tab 키 누르기
+			nLineNumber++;//LineNumber + 1
 		}
-		else if (nLineNumber == 6)
+		else if (nLineNumber == 6)//만약 현재 줄 글자 수가 6이 된다면
 		{
-			cout << endl;
-			nLineNumber = 1;
-			continue;
+			cout << endl;//줄 바꾸기
+			nLineNumber = 1;//줄에 들어간 글자 수 초기화
+			continue;//건너뛰어서 while 문 새로 시작하기
 		}
-		nInsideNumber++;
-		nInsideNumPlus++;
+		nInsideNumber++;//[] 안에 들어가는 숫자 + 1
+		nInsideNumPlus++;//([] 안에 들어가는 숫자 + 1) + 1
 
 	}
 	
