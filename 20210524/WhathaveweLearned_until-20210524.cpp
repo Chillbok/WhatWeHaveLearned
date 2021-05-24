78eedc4dc95a250fd023730f8020aab25dcfdba3
[starfruit0000] <[kylejunho@gmail.com]>
2021-05-24 10:49:46 +0900

    20210524까지 배운 내용(ver 1.1)

--- 20210524/WhathaveweLearned_until-20210524.cpp
+++ 20210524/WhathaveweLearned_until-20210524.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+void Continue_Chapter()
+{
+  cout << "Enter를 눌러서 계속" << endl << endl;
+  system("pause");
+  system("cls");
+}
+
 
 
 void C_and_Cpp_both()//C와 C++에서 모두 사용하는 것들
@@ -39,19 +46,39 @@ void Input_in_C()//C에서의 입력
   출력하면 되는가에 대해서 미리 알 수 있다.
 
   입력을 받을 때는 유저가 얼마만큼을 입력할지를 가늠할 수가 없다.
-
-  아이디 만들때 --> 2 ~ 8글자
+    --> 아이디 만들때: 2글자, 4글자, 5글자, ... / 예상 불가.
   */
   int num;//입력받아서 담아둘 변수.
-  //
-  scanf("%d", &num);
-
+  //사용하기 위해서 프로젝트 속성 --> 언어 --> 준수 모드 --> 아니요 선택
+  scanf_s("%d", &num);
   printf("%d", num);
+}
+
+void Input_in_Cpp()
+{
+  /*
+  C++에서의 출력: cout <<
+  C++에서의 입력: cin >>
+  */
+  //예시
+  cout << "C++에서의 입출력 사용법" << endl;
+  int CppInput;
+  cout << "입력 숫자: "
+  cin >> CppInput;
+  cout << CppInput + 2;
+  /*
+  따라서 마지막에 출력되는 것은 유저가 입력한 숫자에 2를 더한 내용이다.
+  */
 }
 
 int main()
 {
   C_and_Cpp_both();
+  Continue_Chapter();
+  Input_in_C();
+  Continue_Chapter();
+  Input_in_Cpp();
+  Continue_Chapter();
 
   return 0;
 }
