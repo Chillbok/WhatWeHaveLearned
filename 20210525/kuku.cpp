ac3c9443468b54e85c0d276cfa1e8113d05aed0a
[starfruit0000] <[kylejunho@gmail.com]>
2021-05-26 15:53:44 +0900

    구구단 만들기(수단과 방법 가리지 않고)

---
+++ 20210525/kuku.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int x;
+int y;
+int i;
+int j;
+
+void NinetoNine()
+{
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+
+		}
+	}
+}
+int main()
+{
+
+
+	return 0;
+}
